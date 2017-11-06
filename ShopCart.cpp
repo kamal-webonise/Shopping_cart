9acdf1de697effe84f426cf0547929716f10b7d2
kamal-mehra007 <kamal.singh@webonise.com>
2017-11-06 19:34:20 +0530

    Final Code for Shoping Cart

--- ShopCart.cpp
+++ ShopCart.cpp
@@ -19,25 +19,33 @@ class ShopingCart
 		cin>>code;
 		cout<<"\nEnter Product price \n";
 		cin>>price;
+		quan=0;
 	} 
 	
 	int checkout()
 	{
-		
+			
+			if(quan<0)
+				quan=0;
+
 			if(code==1)
 			{
 				cout<<"\npress 1 to avail offer or any other key\n";
 				int key;
 				cin>>key;
+
 				if(key==1)
 				{
-				int temp,cost;
-				cout<<"applicable for 1+1 offer on colgate\n";
-				cost=calc_bill(quan,price);
-				temp=quan/3;
-				cost-=temp*price;
-				return cost;
+					int temp,cost;
+					cout<<"applicable for 1+1 offer on colgate\n";
+					cost=calc_bill(quan,price);
+					temp=quan/3;
+					cost-=temp*price;
+					return cost;
 				}
+				else	
+					return calc_bill(quan,price);
+			
 			}
 			else
 			{	
@@ -73,20 +81,21 @@ int main()
 		pepso.quan=0;
 	}	
 	
-	cout<<"want to shop(y/n)? \n";
+	cout<<"\n\nWant to shop(y/n)? \n";
 	cin>>ch;
 	
 	while(ch=='y')
 	{
 		
-		cout<<"\nPress 1 for purchasing colgate \n";
-		cout<<"\nPress 2 for purchasing pepsodent \n";
+		cout<<"\nPress 1 for adding colgate \n";
+		cout<<"\nPress 2 for adding pepsodent \n";
 		cout<<"\nPress 3 to remove colgate\n";
 		cout<<"\npress 4 to remove pepsodent\n";
 		cout<<"\npress 5 to move to checkout\n";
 		cout<<"\nenter your choice\n";
 		cin>>n;
-		int bill=0 ;
+		int colg_bill=0 ;
+		int pepso_bill=0;
 		switch(n)
 		{	
 			case 1 : 	
@@ -110,10 +119,14 @@ int main()
 				pepso.quan-=quant;
 				break;
 			case 5:
-				bill=colg.checkout();
-				bill+=pepso.checkout();
-				
-				cout<<"total bill is "<<bill<<"\n";
+				colg_bill = colg.checkout();
+				pepso_bill = pepso.checkout();
+
+				cout<<"\n\nNo of Colgate "<<colg.quan<<" for rupees "<<colg_bill<<"\n\n";
+
+				cout<<"\nNo of pepsodent "<<pepso.quan<<" for rupees "<<pepso_bill<<"\n";
+	
+				cout<<"\n\nTOTAL BILL "<<colg_bill+pepso_bill<<"\n";
 				return 1;
 							 
 		}
